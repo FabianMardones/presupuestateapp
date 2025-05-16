7cd12bcfc8e27829627570a45e043c32bb683f4e
Fabian Mardones <fabianmardones94@gmail.com>
2025-05-16 13:54:25 -0400

    integracion con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAZuwhth7MLi31ALYloR61z3iCsIHsOCZ8",
+    authDomain: "presupuestateapp.firebaseapp.com",
+    projectId: "presupuestateapp",
+    storageBucket: "presupuestateapp.firebasestorage.app",
+    messagingSenderId: "570694658685",
+    appId: "1:570694658685:web:0615ef56dd13203a711f14",
+    measurementId: "G-JHC2EV5FRN"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAZuwhth7MLi31ALYloR61z3iCsIHsOCZ8",
+    authDomain: "presupuestateapp.firebaseapp.com",
+    projectId: "presupuestateapp",
+    storageBucket: "presupuestateapp.firebasestorage.app",
+    messagingSenderId: "570694658685",
+    appId: "1:570694658685:web:0615ef56dd13203a711f14",
+    measurementId: "G-JHC2EV5FRN"
+  }
 };
 
 /*
