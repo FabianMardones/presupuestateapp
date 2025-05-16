faf2fa66831779727e04335eb844ac535a696b97
Fabian Mardones <fabianmardones94@gmail.com>
2025-05-16 12:19:10 -0400

    creación repo

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.web.presupuestateapp',
+  appName: 'PresupuestateApp',
+  webDir: 'www'
+};
+
+export default config;
